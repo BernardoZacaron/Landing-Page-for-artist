43381bd1ceb9d28d28ecb7a46a504c6e795117d1
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-01-18 16:04:35 -0300

    Expand book divs onclick implemented

---
+++ LandingPageArtista/public_html/scripts/script1.js
@@ -0,0 +1,38 @@
+const livroEsquerda = document.getElementById('livro-esquerda');
+const livroDireita = document.getElementById('livro-direita');
+
+function esquerda(){
+    if(livroEsquerda.classList.contains('ativo')){
+        livroEsquerda.classList.remove('ativo');
+        livroEsquerda.classList.add('desativo');
+    }else{
+        livroEsquerda.classList.remove('desativo');
+        livroEsquerda.classList.add('ativo');
+        
+        livroDireita.classList.remove('ativo');
+        livroDireita.classList.add('desativo'); 
+    }
+        
+}
+function direita(){
+    if(livroDireita.classList.contains('ativo')){
+        livroDireita.classList.remove('ativo');
+        livroDireita.classList.add('desativo');
+    }else{
+        livroDireita.classList.remove('desativo');
+        livroDireita.classList.add('ativo');
+        
+        livroEsquerda.classList.remove('ativo');
+        livroEsquerda.classList.add('desativo');
+    } 
+}
+
+window.onload=function(){
+    livroEsquerda.onclick = function (){
+        esquerda();
+    };
+    
+    livroDireita.onclick = function (){
+        direita();
+    };
+};
